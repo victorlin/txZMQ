169a62e2d6e9a154de35d763f6a14cac1dcca479
Victor Lin <bornstub@gmail.com>
2012-01-24 03:17:05 +0800

    Add names back to package

--- txzmq/__init__.py
+++ txzmq/__init__.py
@@ -1,3 +1,17 @@
 """
 ZeroMQ integration into Twisted reactor.
 """
+from txzmq.connection import ZmqConnection
+from txzmq.factory import ZmqFactory
+from txzmq.pubsub import ZmqPubConnection, ZmqSubConnection
+from txzmq.reqrep import ZmqXREQConnection, ZmqXREPConnection
+
+
+__all__ = [
+    'ZmqConnection', 
+    'ZmqFactory',
+    'ZmqPubConnection', 
+    'ZmqSubConnection', 
+    'ZmqXREQConnection',
+    'ZmqXREPConnection'
+]
