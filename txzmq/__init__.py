d284d7baff50c1f4fd5218eff86fe78ae1cc864d
Victor Lin <bornstub@gmail.com>
2012-02-26 01:51:48 +0800

    Fix missing imports for req/rep in __init__.py

--- txzmq/__init__.py
+++ txzmq/__init__.py
@@ -6,6 +6,7 @@ from txzmq.factory import ZmqFactory
 from txzmq.pair import ZmqPairConnection
 from txzmq.pubsub import ZmqPubConnection, ZmqSubConnection
 from txzmq.pushpull import ZmqPushConnection, ZmqPullConnection
+from txzmq.reqrep import ZmqReqConnection, ZmqRepConnection
 from txzmq.reqrep import ZmqXREQConnection, ZmqXREPConnection
 
 __all__ = [
